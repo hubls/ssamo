a0ab25a90a82b2572510aa182cf52dc10f77d855
leesangjun <hubls@naver.com>
2024-01-16 17:20:43 +0900

    test

--- routes/webtoonsRouter.js
+++ routes/webtoonsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/webtoons', (req, res) => {
-  res.send('안녕');
+  res.send('test');
 });
 
 module.exports = router;
