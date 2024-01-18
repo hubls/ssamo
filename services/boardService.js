50cac5a12b695616fcf973f5fe4eabbe111a789d
leesangjun <hubls@naver.com>
2024-01-18 16:32:21 +0900

    fix: 방문자순 순으로 사이트 출력

--- services/boardService.js
+++ services/boardService.js
@@ -2,7 +2,7 @@ const MySQLRepository = require('../db/MySQLRepository');
 
 class BoardService {
   static async getAllData(boardName) {
-    const sql = `SELECT * FROM ${boardName}`
+    const sql = `SELECT * FROM ${boardName} ORDER BY visitorCount DESC`;
     const result = await MySQLRepository.executeQuery(sql);
 
     return result;
